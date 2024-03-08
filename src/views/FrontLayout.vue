027b517dc7c344a6c697a130eb0da1ecde6941a9
PayRoom <water38198@gmail.com>
2024-03-08 21:13:18 +0800

    新增Footer

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavBarComp from '@/components/front/NavBarComp.vue';
-
+import FooterComp from '@/components/front/FooterComp.vue';
 </script>
 
 
 <template>
     <div class="bg-secondary">
-        <NavBarComp></NavBarComp>
+        <NavBarComp />
         <RouterView />
+        <FooterComp />
     </div>
 </template>
