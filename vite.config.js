e7e808b657af89ee1ee91645320321580e3610b7
PayRoom <water38198@gmail.com>
2024-03-08 21:20:05 +0800

    VITE編譯錯誤修正

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,4 +21,21 @@ export default defineConfig({
   },
   /* global process */
   base: process.env.NODE_ENV === 'production' ? '/GDiner/' : '/',
+      // 錯誤修正 參考:https://israynotarray.com/other/20220928/1377668819/
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  },
 })
