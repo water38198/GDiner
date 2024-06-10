ae8445b9b8fb2ecfb2d1662c595d0b3b3f369ee1
PayRoom <water38198@gmail.com>
2024-06-10 09:43:59 +0800

    Loading SW2 axios 全域設定

--- src/main.js
+++ src/main.js
@@ -2,11 +2,24 @@ import '@unocss/reset/eric-meyer.css';
 import 'virtual:uno.css';
 import './assets/scss/main.scss';
 
+// SweetAlert2
+import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/src/sweetalert2.scss';
+const options = {
+  confirmButtonColor: '#3D081B',
+}
+
+// vue-axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Loading
+import Loading from 'vue-loading-overlay';
 import 'vue-loading-overlay/dist/css/index.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+
 //VeeValidate
 import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate';
 import * as AllRules from '@vee-validate/rules';
@@ -29,8 +42,13 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueSweetalert2, options);
+app.use(VueAxios, axios);
+
+app.component('VLoading', Loading);
 // 掛載 Global 的 VeeValidate 元件
 app.component('VField', Field);
 app.component('VForm', Form);
 app.component('ErrorMessage', ErrorMessage);
+
 app.mount('#app');
