c737c1a0a8d730f16273ee809778179f65b3ba4f
PayRoom <water38198@gmail.com>
2024-06-15 00:25:19 +0800

    admin layout option api

--- src/views/AdminLayout.vue
+++ src/views/AdminLayout.vue
@@ -1,12 +1,7 @@
-<script setup>
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { useRouter, RouterView } from 'vue-router';
-import Loading from 'vue-loading-overlay';
+<!-- <script setup>
 import AdminNavbar from '@/components/admin/AdminNavbar.vue';
 
-const { VITE_URL } = import.meta.env;
+
 const router = useRouter();
 const isChecked = ref(false);
 const isLoading = ref(false);
@@ -39,10 +34,62 @@ function checkAdmin() {
 onMounted(() => {
   checkAdmin();
 })
+</script> -->
+<script>
+import AdminNavbar from '@/components/admin/AdminNavbar.vue';
+
+const { VITE_URL } = import.meta.env;
+
+export default {
+  components:{ AdminNavbar},
+  data() {
+    return {
+      isChecked: false,
+      isLoading: false,
+    }
+  },
+  methods: {
+    async checkLogin() {
+      this.isLoading = true;
+      const token = document.cookie.split("; ").find((row) => row.startsWith("myToken="))?.split("=")[1];
+      this.$http.defaults.headers.common.Authorization = token;
+      try {
+        const res = await this.$http.post(`${VITE_URL}/v2/api/user/check`);
+        if (res.data.success) {
+          this.isChecked = true;
+          this.$swal({
+            position: 'bottom-end',
+            toast: true,
+            icon: 'success',
+            title: '驗證成功',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        };
+      } catch (err) {
+        this.isChecked = false;
+        this.$swal({
+          title: '請重新登入',
+          icon: "error",
+          text: `${err.response.data.message || '登入錯誤'}`,
+          didClose: () => {
+            this.$router.push('/login');
+          }
+        })
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.checkLogin();
+  }
+}
 </script>
 
+
 <template>
-  <Loading :active="isLoading" :full-page="true" />
+  <VLoading :active="isLoading" :full-page="true" />
   <div class="grid grid-cols-[20rem_1fr] gap-2 h-screen">
     <AdminNavbar />
     <div v-if="isChecked">
